ccc2dfd2c5bdff0a1c2b0318fd58810d1cbac928
Kawsar Aleev <144724052+KawsarAleev@users.noreply.github.com>
2025-03-11 21:41:56 +0600

    Add files via upload

---
+++ components/ui/chart.tsx
@@ -0,0 +1,22 @@
+import {
+  BarChart as RechartsBarChart,
+  Bar,
+  LineChart as RechartsLineChart,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts"
+
+export {
+  RechartsBarChart as BarChart,
+  Bar,
+  RechartsLineChart as LineChart,
+  Line,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+}
+
